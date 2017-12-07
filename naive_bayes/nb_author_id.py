3dd748ba9af1dd309d344dba1a96315c9d4ff420
sidcoder <siddharthchordia@Siddharths-MacBook-Pro.local>
2017-12-06 19:02:15 -0500

    Training time and prediction time per list element
    Training time 1.2x10-4
    Prediction timw 1.1x10-4

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -35,10 +35,12 @@ clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
+print(len(labels_train))
 
 t1 = time()
 pred = clf.predict(features_test)
 print "prediction time:", round(time()-t1, 3), "s"
+print(len(features_test))
 
 accuracy = clf.score(features_test,labels_test)
 print(accuracy)
